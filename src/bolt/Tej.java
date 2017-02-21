a2373260157ff3de71fc78e8f4b3dae56cfc62b0
László Székely-Tóth <notisracer@gmail.com>
2017-02-21 19:57:02 +0100

    task 1

---
+++ src/bolt/Tej.java
@@ -0,0 +1,54 @@
+package bolt;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Tej {
+    private int urtartalom;
+    private String gyarto;
+    private Date szavatossagiIdo;
+    private double zsirtartalom;
+    private long ar;
+
+    public Tej(int urtartalom,
+               String gyarto,
+               Date szavatossagiIdo,
+               double zsirtartalom,
+               long ar) {
+
+        this.urtartalom = urtartalom;
+        this.gyarto = gyarto;
+        this.szavatossagiIdo = szavatossagiIdo;
+        this.zsirtartalom = zsirtartalom;
+        this.ar = ar;
+    }
+
+    public boolean joMeg() {
+        return Calendar.getInstance().getTime().compareTo(szavatossagiIdo) < 0;
+    }
+
+    public int getUrtartalom() {
+        return urtartalom;
+    }
+
+    public String getGyarto() {
+        return gyarto;
+    }
+
+    public Date getSzavatossagiIdo() {
+        return szavatossagiIdo;
+    }
+
+    public double getZsirtartalom() {
+        return zsirtartalom;
+    }
+
+    public long getAr() {
+        return ar;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
