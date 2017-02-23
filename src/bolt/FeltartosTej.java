fb0c8aac9e5c4c28bcbbe9feba28482ea9992210
László Székely-Tóth <notisracer@gmail.com>
2017-02-23 22:00:40 +0100

    task 3

---
+++ src/bolt/FeltartosTej.java
@@ -0,0 +1,9 @@
+package bolt;
+
+import java.util.Date;
+
+public class FeltartosTej extends Tej {
+    public FeltartosTej(long vonalkod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
+        super(vonalkod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
+    }
+}
--- src/bolt/TartosTej.java
+++ src/bolt/TartosTej.java
@@ -1,4 +1,9 @@
 package bolt;
 
-public class TartosTej {
+import java.util.Date;
+
+public class TartosTej extends Tej {
+    public TartosTej(long vonalkod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
+        super(vonalkod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
+    }
 }
