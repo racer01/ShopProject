f044611447792697e391631196d20d2a5410fc35
László Székely-Tóth <notisracer@gmail.com>
2017-02-23 23:20:52 +0100

    task 3.5

--- src/bolt/Tej.java
+++ src/bolt/Tej.java
@@ -1,18 +1,14 @@
 package bolt;
 
-import java.util.Calendar;
 import java.util.Date;
 
-public abstract class Tej {
+public abstract class Tej extends Elelmiszer {
     public static final int LITER = 1000;
     public static final int FELLITER = 500;
     public static final int POHAR = 200;
     public static final double ZSIROS = 3.5;
     public static final double FELZSIROS = 2.8;
-    protected Long vonalkod;
     protected int urtartalom;
-    protected String gyarto;
-    protected Date szavatossagiIdo;
     protected double zsirtartalom;
 
     public Tej(long vonalkod,
@@ -20,45 +16,16 @@ public abstract class Tej {
                String gyarto,
                Date szavatossagiIdo,
                double zsirtartalom) {
-        this.vonalkod = vonalkod;
+        super(vonalkod, gyarto, szavatossagiIdo);
         this.urtartalom = urtartalom;
-        this.gyarto = gyarto;
-        this.szavatossagiIdo = szavatossagiIdo;
         this.zsirtartalom = zsirtartalom;
-    }
-
-    public Long getVonalkod() {
-        return vonalkod;
-    }
-
-    public boolean joMeg() {
-        return Calendar.getInstance().getTime().compareTo(szavatossagiIdo) < 0;
     }
 
     public int getUrtartalom() {
         return urtartalom;
     }
 
-    public String getGyarto() {
-        return gyarto;
-    }
-
-    public Date getSzavatossagiIdo() {
-        return szavatossagiIdo;
-    }
-
     public double getZsirtartalom() {
         return zsirtartalom;
-    }
-
-    @Override
-    public String toString() {
-        String zsir = "";
-        if (zsirtartalom == ZSIROS) {
-            zsir = String.format("%s ", "zsíros");
-        } else if (zsirtartalom == FELZSIROS) {
-            zsir = String.format("%s ", "félzsíros");
-        }
-        return String.format("%s %s tej (%f%%) %fl", gyarto, zsir, zsirtartalom, (float) urtartalom / 1000);
     }
 }
--- src/bolt/tej/FeltartosTej.java
+++ src/bolt/tej/FeltartosTej.java
@@ -8,4 +8,15 @@ public class FeltartosTej extends Tej {
     public FeltartosTej(long vonalkod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
         super(vonalkod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
     }
+
+    @Override
+    public String toString() {
+        String zsir = "";
+        if (zsirtartalom == ZSIROS) {
+            zsir = "zsíros";
+        } else if (zsirtartalom == FELZSIROS) {
+            zsir = "félzsíros";
+        }
+        return String.format("%s %s, féltartós tej (%f%%) %fl", gyarto, zsir, zsirtartalom, (float) urtartalom / 1000);
+    }
 }
